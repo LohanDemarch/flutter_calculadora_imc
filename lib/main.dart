34a0835d0487cccd6c3273390885ebbef95d29ad
Lohan Gabriel Moraes Demarch <39281195+LohanDemarch@users.noreply.github.com>
2020-02-19 22:29:54 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          Icon(Icons.person, size: 120,color: Colors.green)
+        ],
+      ),
+    );
+  }
+}
